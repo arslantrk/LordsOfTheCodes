0e41879b2665fd9174aec11e2409b17c43d67e90
Ramazan Arslantürk <ramazan.1997.1905@hotmail.com>
2021-02-22 00:51:33 +0300

    Add Result Mechanizm in the Core

---
+++ Business/Abstract/ICarService.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ICarService
+    {
+        List<Car> GetAll();
+        Car GetById(int id);
+        List<Car> GetListByBrandId(int brandid);
+        List<Car> GetListByColorId(int colorid);
+        List<Car> GetBy(Car car);
+        void Add(Car car);
+        void Delete(Car car);
+        void Update(Car car);
+
+    }
+}
